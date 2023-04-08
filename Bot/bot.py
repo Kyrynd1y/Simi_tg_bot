dd5a29c4c480c4e6cb116920f5a054f0d670c8af
fomichev <fomkirich@gmail.com>
2023-04-08 18:24:25 +0500

    Криво отвечает 1

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,38 +1,27 @@
-import json
-import logging
-from telegram.ext import MessageHandler, CommandHandler, Application, filters
-import apiai
+import openai
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-BOT_TOKEN = '6217430570:AAE2I1NZYFIUjzYFCXZtYyTHi31rSR79RDE'
-project_id = 'simi-ukah'
+openai.api_key = "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"
+token = '6217430570:AAE2I1NZYFIUjzYFCXZtYyTHi31rSR79RDE'
 
-
-def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
-
-
-def textMessage(bot, update):
-    request = apiai.ApiAI(project_id).text_request()  # Токен API к Dialogflow
-    request.lang = 'ru'  # На каком языке будет послан запрос
-    request.session_id = 'BatlabAIBot'  # ID Сессии диалога (нужно, чтобы потом учить бота)
-    request.query = update.message.text  # Посылаем запрос к ИИ с сообщением от юзера
-    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-    response = responseJson['result']['fulfillment']['speech']  # Разбираем JSON и вытаскиваем ответ
-    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
-    if response:
-        bot.send_message(chat_id=update.message.chat_id, text=response)
-    else:
-        bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
+bot = Bot(token=token)
+dp = Dispatcher(bot)
 
 
-def main():
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler('start', startCommand))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, textMessage()))
-    application.run_polling()
+@dp.message_handler()
+async def send(message: types.Message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.9,
+        max_tokens=1000,
+        top_p=0.7,
+        frequency_penalty=0.0
+    )
+    await message.answer(response['choices'][0]['text'])
 
 
 if __name__ == '__main__':
-    main()
-
-#
+    executor.start_polling(dp)
